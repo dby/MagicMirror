a94051fe3fbb8f3bbfbd3de185eec7d359f76c41
sys <1052856576@qq.com>
2015-12-01 23:01:43 +0800

    Initial Commit

---
+++ MagicMirror2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MagicMirror2
+//
+//  Created by sys on 15/12/1.
+//  Copyright © 2015年 sys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MagicMirror2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MagicMirror2
+//
+//  Created by sys on 15/12/1.
+//  Copyright © 2015年 sys. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
